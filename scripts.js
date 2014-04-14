14680a997185621b875e04ecb2de9fc05a22f9be
Sylvain Dorey <dorey.sylvain@gmail.com>
2014-04-14 14:24:54 +0200

    workink on markers

--- scripts.js
+++ scripts.js
@@ -1,26 +1,43 @@
-function init() {
-    map = new OpenLayers.Map("map");
-    var mapnik = new OpenLayers.Layer.OSM();
-    var fromProjection = new OpenLayers.Projection("EPSG:4326");
-    var toProjection = new OpenLayers.Projection("EPSG:900913");
-    var position = new OpenLayers.LonLat(102.37, 17.58).transform(fromProjection, toProjection);
-    var zoom = 6;
+var init = function() {
+  /* Map and base layer*/
+  map = new OpenLayers.Map("map");
+  var mapnik = new OpenLayers.Layer.OSM();
+  var fromProjection = new OpenLayers.Projection("EPSG:4326");
+  var toProjection = new OpenLayers.Projection("EPSG:900913");
+  var position = new OpenLayers.LonLat(-1.05, 49.05).transform(fromProjection, toProjection);
+  var zoom = 9;
+  map.addLayer(mapnik);
 
-    map.addLayer(mapnik);
-    var markers = new OpenLayers.Layer.Markers("Markers");
-    map.addLayer(markers);
-    markers.addMarker(new OpenLayers.Marker(position));
+  /* Communes boundaries */
+  var geojson_layer = new OpenLayers.Layer.Vector("GeoJSON", {
+      strategies: [new OpenLayers.Strategy.Fixed()],
+      protocol: new OpenLayers.Protocol.HTTP({
+          url: "manche.json",
+          format: new OpenLayers.Format.GeoJSON()
+      }),
+      style: {
+        fill: false,
+        strokeWidth: 0.2
+      }
+  });
+  map.addLayer(geojson_layer);
 
-    var lgpx = new OpenLayers.Layer.Vector("Laos track"{
-            strategies: [new OpenLayers.Strategy.Fixed()],
-            protocol: new OpenLayers.Protocol.HTTP({
-                url: "laos.gpx",
-                format: new OpenLayers.Format.GPX()
-            }),
-            style: {strokeColor: "green", strokeWidth: 5, strokeOpacity: 0.5},
-            projection: new OpenLayers.Projection("EPSG:4326")
-    });
-    map.addLayer(lgpx);
+  /* Points Layer */
 
-    map.setCenter(position, zoom);
+  var symbolizer = OpenLayers.Util.applyDefaults({
+    pointRadius: 20,
+    fillColor: "red"
+  }, OpenLayers.Feature.Vector.style["default"]));
+
+  var styleMap = new OpenLayers.StyleMap({"default": "symbolizer"});
+
+
+  var point = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(-1.37, 49.38).transform(fromProjection, toProjection));
+  var points_layer = new OpenLayers.Layer.Vector("Points Layer", {
+      styleMap: styleMap
+  });
+  map.addLayer(points_layer);
+  points_layer.addFeatures([point]);
+
+  map.setCenter(position, zoom);
 }
