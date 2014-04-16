f264069eba0bb35f3c46f97291a36e96c39c7f58
Sylvain Dorey <dorey.sylvain@gmail.com>
2014-04-16 13:56:25 +0200

    ascend mapping

--- scripts.js
+++ scripts.js
@@ -26,24 +26,36 @@ var init = function() {
 
   var styleMap = new OpenLayers.StyleMap({
     "default": new OpenLayers.Style({
+      "pointRadius": 6,
+      "strokeWidth": 1
     },{
       rules: [
       new OpenLayers.Rule({
         filter: new OpenLayers.Filter.Comparison({
-          type: OpenLayers.Filter.Comparison.LESS_THAN,
-          property: "type",
-          value: 1
+          type: OpenLayers.Filter.Comparison.BETWEEN,
+          lowerBoundary: 0,
+          upperBoundary: 10,
+          property: "nombre",
         }),
         symbolizer: {
-          "pointRadius": 12,
           "fillColor": "yellow"
+        }
+      }),
+      new OpenLayers.Rule({
+        filter: new OpenLayers.Filter.Comparison({
+          type: OpenLayers.Filter.Comparison.BETWEEN,
+          lowerBoundary: 11,
+          upperBoundary: 100,
+          property: "nombre",
+        }),
+        symbolizer: {
+          "fillColor": "orange"
         }
       }),
       new OpenLayers.Rule({
         elseFilter: true,
         symbolizer: {
           "fillColor": "red",
-          "pointRadius": 8.2
         }
       })
       ]
@@ -53,22 +65,16 @@ var init = function() {
   });
 
   var points = [];
-  var coord = [{lon: -1.61, lat: 49.63}, {lon: -1.22, lat: 49.54}, {lon: -1.33, lat: 49.40}, {lon: -1.15, lat: 49.10}]
-  for(var i = 0 ; i < coord.length ; i ++) {
-    points[i] = new OpenLayers.Feature.Vector(new OpenLayers.Geometry
-                                                .Point(coord[i].lon, coord[i].lat).transform(fromProjection, toProjection),
-                                                {message: 'hello', type: parseInt(Math.random()*3)});
-  }
 
   var points_layer = new OpenLayers.Layer.Vector("Points Layer", {
       styleMap: styleMap,
       eventListeners: {
-        'featureselected':function(evt){
+        'featureselected':function(evt) {
           var feature = evt.feature;
           var popup = new OpenLayers.Popup.FramedCloud("popup",
               OpenLayers.LonLat.fromString(feature.geometry.toShortString()),
               null,
-              feature.attributes.message +" <br>" + feature.attributes.location,
+              feature.attributes.commune +" <br>" + feature.attributes.nombre + ' personne(s)',
               null,
               true,
               null
@@ -95,6 +101,73 @@ var init = function() {
 
   map.addControl(selector);
   map.addLayer(points_layer);
+
+  var Dico = {};
+  var i = 0;
+  Dico = read_ascend("ascendants.csv");
+  for(var key in Dico) {
+    if(Dico.hasOwnProperty(key)) {
+      points[i] = new OpenLayers.Feature.Vector(new OpenLayers.Geometry
+                                                .Point(Dico[key][1], Dico[key][2]).transform(fromProjection, toProjection),
+                                                {commune: key, nombre: Dico[key][0]});
+      i++;
+    }
+  }
   points_layer.addFeatures(points);
   map.setCenter(position, zoom);
 }
+
+//
+// process_file
+//  allTest: String
+//
+// Take a text whith multiple lines and words separated by semi-column
+// and return a processed Dico.
+
+var process_file = function(allText) {
+  console.log('hello');
+  var text = allText.split("\n");
+  var Dico = {};
+  for (var i = 0 ; i < text.length ; i++) {
+    var line = text[i].split(";");
+    if (Dico[line[1]]) {
+      Dico[line[1]][0]++;
+    }
+    else {
+      Dico[line[1]] = [];
+      Dico[line[1]][0] = 1;
+      Dico[line[1]][1] = line[2];
+      Dico[line[1]][2] = line[3];
+    }
+  }
+  return Dico;
+}
+
+//
+// read_ascend
+//  url: String
+//
+// Read the ascendant.csv file and return a processed Dico from it.
+
+var read_ascend = function(fileURL) {
+  /* We assume that the data to read is a csv file (actually ;) of the following format :
+    date; commune; longitude; latitude 
+    The data is a lines 2D array*/
+  var rawFile = new XMLHttpRequest();
+  var Dico = {};
+  var lines;
+  rawFile.open("GET", fileURL, false);
+  rawFile.onreadystatechange = function ()
+  {
+      if(rawFile.readyState === 4)
+      {
+          if(rawFile.status === 200 || rawFile.status == 0)
+          {
+            var allText = rawFile.responseText;
+            Dico = process_file(allText);
+          }
+      }
+  }
+  rawFile.send(null);
+  return Dico;
+} 
